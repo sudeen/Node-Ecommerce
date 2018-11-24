f2fd0ee52da4f8d9bdfcdbcad33866c67427d955
sudeen <ranjitkarsudeen14@gmail.com>
2018-11-24 18:42:04 +1100

    added user schema with moongoose and bcrypt nodejs

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 var express = require('express');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var User = require('./models/user');
 
 var app = express();
 
-app.get('/', function(req, res){
-    var name = "Sudin Ranjitkar";
-    res.json("My name is " +name);
+mongoose.connect('mongodb://root:abc123@ds115434.mlab.com:15434/ecommerce', function (err) {
+    if (err) {
+        console.log(err);
+    }
+    else{
+        console.log("Connected to database");
+    }
+
 })
 
-app.listen(3000, function(err){
-    if(err) throw err;
-    console.log("Server is Running on port 3000");
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.post('/create-user', function(req, res, next){
+    var user = new User();
+    user.profile.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email;
+
+    user.save(function(err){
+        if(err) next(err);
+
+        res.json('Successfully created new user');
+
+    });
+}); 
+
+app.listen(9000, function (err) {
+    if (err) throw err;
+    console.log("Server is Running on port 9000");
 });
