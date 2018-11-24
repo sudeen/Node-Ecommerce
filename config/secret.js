a47834dce8b4938fe7b90eb4b8344dd95acf424c
sudeen <ranjitkarsudeen14@gmail.com>
2018-11-24 22:51:06 +1100

    changed db url

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  database: 'mongodb://root:abc123@ds037095.mongolab.com:37095/ecommerce',
+  database: 'mongodb://root:abc123@ds115434.mlab.com:15434/ecommerce',
   port: 9000,
   secretKey: "sudin123"
 }
