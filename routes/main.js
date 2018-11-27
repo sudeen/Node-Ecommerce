2ab84016489328b4aaedcb23314ab830cb8f461b
sudeen <ranjitkarsudeen14@gmail.com>
2018-11-27 18:41:19 +1100

    added category section open with product id

--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,29 @@
 var router = require('express').Router();
 var User = require('../models/user');
+var Product = require('../models/product');
 
 
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('main/home');
 });
 
-router.get('/about', function(req, res) {
+router.get('/about', function (req, res) {
   res.render('main/about');
 });
 
-router.get('/users', function(req, res){
-  User.find({}, function(err, users){
-    res.json(users);
-  })
+router.get('/products/:id', function (req, res, next) {
+  Product
+    .find({
+      category: req.params.id
+    })
+    .populate('category')
+    .exec(function (err, products) {
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products
+      });
+    });;
 })
 
 
