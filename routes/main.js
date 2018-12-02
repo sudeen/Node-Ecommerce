9b7d683472aea294d20cd445bcbf9d4c5d1a91c6
sudeen <ranjitkarsudeen14@gmail.com>
2018-12-02 23:44:10 +1100

    added custom.js

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,30 @@ var router = require('express').Router();
 var User = require('../models/user');
 var Product = require('../models/product');
 
+
+function paginate(req, res, next) {
+
+  var perPage = 9;
+  var page = req.params.page;
+
+  Product
+    .find()
+    .skip( perPage * page)
+    .limit( perPage )
+    .populate('category')
+    .exec(function(err, products) {
+      if (err) return next(err);
+      Product.count().exec(function(err, count) {
+        if (err) return next(err);
+        res.render('main/product-main', {
+          products: products,
+          pages: count / perPage
+        });
+      });
+    });
+
+}
+
 /* Product.createMapping(function (err, mapping) {
   if (err) {
     console.log("error creating mapping");
@@ -27,29 +51,14 @@ stream.on('error', function (err) {
   console.log(err);
 }); */
 
-router.get('/', function (req, res, next) {
-  if (req.user) {
-    var perPage = 9;
-    var page = req.params.page;
+router.get('/', function(req, res, next) {
 
-    Product
-      .find()
-      .skip(perPage * page)
-      .limit(perPage)
-      .populate('category')
-      .exec(function (err, products) {
-        if (err) return next(err);
-        Product.count().exec(function (err, count) {
-          if (err) return next(err);
-          res.render('/main/product-main', {
-            products: products,
-            pages: count / perPage
-          });
-        });
-      }); 
+  if (req.user) {
+    paginate(req, res, next);
   } else {
     res.render('main/home');
   }
+
 });
 
 router.get('/about', function (req, res) {
@@ -77,6 +86,24 @@ router.get('/product/:id', function (req, res, next) {
     if (err) return next(err);
     res.render('main/product', {
       product: product
+    });
+  });
+});
+
+router.post('/product/:product_id', function(req, res, next) {
+  Cart.findOne({ owner: req.user._id }, function(err, cart) {
+    // push is used to push the items into the cart
+    cart.items.push({
+      item: req.body.product_id,
+      price: parseFloat(req.body.priceValue),
+      quantity: parseInt(req.body.quantity)
+    });
+
+    cart.total = (cart.total + parseFloat(req.body.priceValue)).toFixed(2);
+
+    cart.save(function(err) {
+      if (err) return next(err);
+      return res.redirect('/cart');
     });
   });
 });
