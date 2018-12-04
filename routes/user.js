56f4a127bb68a75226d5eeb54efa5e2e3e774357
sudeen <ranjitkarsudeen14@gmail.com>
2018-12-04 15:52:14 +1100

    updated profile api

--- routes/user.js
+++ routes/user.js
@@ -17,15 +17,15 @@ router.post('/login', passport.authenticate('local-login', {
   failureFlash: true
 }));
 
-router.get('/profile', function(req, res, next) {
-  User.findOne({ _id: req.user._id }, function(err, user) {
-    if (err) return next(err);
+router.get('/profile', passportConf.isAuthenticated, function(req, res, next) {
+  User
+    .findOne({ _id: req.user._id })
+    .populate('history.item')
+    .exec(function(err, foundUser) {
+      if (err) return next(err);
 
-    res.render('accounts/profile', { user: user });
-
-  });
-
-
+      res.render('accounts/profile', { user: foundUser });
+    });
 });
 
 router.get('/signup', function(req, res, next) {
