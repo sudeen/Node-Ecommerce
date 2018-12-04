1d17444e4bc5706a85108362077d896af5be655c
sudeen <ranjitkarsudeen14@gmail.com>
2018-12-04 15:33:02 +1100

    stripe payment test success

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,9 @@ var User = require('../models/user');
 var Product = require('../models/product');
 var Cart = require('../models/cart');
 
-var stripe = require('stripe')('sk_test_DTpy2YvAHF53EdX47GTqnrEK');
+var async = require('async');
+
+var stripe = require('stripe') ('sk_test_DTpy2YvAHF53EdX47GTqnrEK');
 
 function paginate(req, res, next) {
 
@@ -28,7 +30,7 @@ function paginate(req, res, next) {
 
 }
 
-/* Product.createMapping(function (err, mapping) {
+/* Product.createMapping(function(err, mapping) {
   if (err) {
     console.log("error creating mapping");
     console.log(err);
@@ -41,60 +43,33 @@ function paginate(req, res, next) {
 var stream = Product.synchronize();
 var count = 0;
 
-stream.on('date', function () {
+stream.on('data', function() {
   count++;
 });
 
-stream.on('close', function () {
-  console.log("Indexed", + count + "documents");
+stream.on('close', function() {
+  console.log("Indexed " + count + " documents");
 });
 
-stream.on('error', function (err) {
+stream.on('error', function(err) {
   console.log(err);
 }); */
 
-router.get('/', function(req, res, next) {
-
-  if (req.user) {
-    paginate(req, res, next);
-  } else {
-    res.render('main/home');
-  }
-
-});
-
-router.get('/about', function (req, res) {
-  res.render('main/about');
-});
-
-router.get('/products/:id', function (req, res, next) {
-  Product
-    .find({
-      category: req.params.id
-    })
-    .populate('category')
-    .exec(function (err, products) {
+router.get('/cart', function(req, res, next) {
+  Cart
+    .findOne({ owner: req.user._id })
+    .populate('items.item')
+    .exec(function(err, foundCart) {
       if (err) return next(err);
-      res.render('main/category', {
-        products: products
+      res.render('main/cart', {
+        foundCart: foundCart,
+        message: req.flash('remove')
       });
     });
 });
 
-router.get('/product/:id', function (req, res, next) {
-  Product.findById({
-    _id: req.params.id
-  }, function (err, product) {
-    if (err) return next(err);
-    res.render('main/product', {
-      product: product
-    });
-  });
-});
-
 router.post('/product/:product_id', function(req, res, next) {
   Cart.findOne({ owner: req.user._id }, function(err, cart) {
-    // push is used to push the items into the cart
     cart.items.push({
       item: req.body.product_id,
       price: parseFloat(req.body.priceValue),
@@ -110,31 +85,83 @@ router.post('/product/:product_id', function(req, res, next) {
   });
 });
 
-router.post('/remove', function(req, res, next){
-  Cart.findOne({ owner: req.user._id }, function(err, foundCart){
+
+router.post('/remove', function(req, res, next) {
+  Cart.findOne({ owner: req.user._id }, function(err, foundCart) {
     foundCart.items.pull(String(req.body.item));
 
     foundCart.total = (foundCart.total - parseFloat(req.body.price)).toFixed(2);
-    foundCart.save(function(err, found){
-      if(err) return next(err);
+    foundCart.save(function(err, found) {
+      if (err) return next(err);
       req.flash('remove', 'Successfully removed');
       res.redirect('/cart');
     });
   });
 });
 
-router.get('/cart', function(req, res, next) {
-  Cart
-    .findOne({ owner: req.user._id })
-    .populate('items.item')
-    .exec(function(err, foundCart) {
+
+router.post('/search', function(req, res, next) {
+  res.redirect('/search?q=' + req.body.q);
+});
+
+router.get('/search', function(req, res, next) {
+  if (req.query.q) {
+    Product.search({
+      query_string: { query: req.query.q}
+    }, function(err, results) {
+      results:
       if (err) return next(err);
-      res.render('main/cart', {
-        foundCart: foundCart,
-        message: req.flash('remove')
+      var data = results.hits.hits.map(function(hit) {
+        return hit;
+      });
+      res.render('main/search-result', {
+        query: req.query.q,
+        data: data
+      });
+    });
+  }
+});
+
+router.get('/', function(req, res, next) {
+
+  if (req.user) {
+    paginate(req, res, next);
+  } else {
+    res.render('main/home');
+  }
+
+});
+
+router.get('/page/:page', function(req, res, next) {
+  paginate(req,res,next);
+});
+
+router.get('/about', function(req, res) {
+  res.render('main/about');
+});
+
+router.get('/products/:id', function(req, res, next) {
+  Product
+    .find({ category: req.params.id })
+    .populate('category')
+    .exec(function(err, products) {
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products
       });
     });
 });
+
+
+router.get('/product/:id', function(req, res, next) {
+  Product.findById({ _id: req.params.id }, function(err, product) {
+    if (err) return next(err);
+    res.render('main/product', {
+      product: product
+    });
+  });
+});
+
 
 
 router.post('/payment', function(req, res, next) {
@@ -146,7 +173,7 @@ router.post('/payment', function(req, res, next) {
   }).then(function(customer) {
     return stripe.charges.create({
       amount: currentCharges,
-      currency: 'aud',
+      currency: 'usd',
       customer: customer.id
     });
   }).then(function(charge) {
@@ -185,6 +212,5 @@ router.post('/payment', function(req, res, next) {
 
 
 });
-
 
 module.exports = router;
